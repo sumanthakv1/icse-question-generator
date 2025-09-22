8414de97ff769546ec99fbd59c709cf45a90a1eb
sumanthakv1 <sumanthakv@gmail.com>
2025-09-22 20:18:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, send_file, redirect, url_for
-import random
+import streamlit as st
 import pandas as pd
+import random
 import io
 
-app = Flask(__name__)
+st.title('ICSE Mock Paper Generator')
 
 QUESTION_BANK = {
     '9': {
@@ -20,49 +20,31 @@ QUESTION_BANK = {
     }
 }
 
-generated_questions = []
+classes = ['9', '10', '11', '12']
+class_selected = st.selectbox('Select Class', classes)
+subjects = list(QUESTION_BANK.get(class_selected, {}).keys())
+if subjects:
+    subject_selected = st.selectbox('Select Subject', subjects)
+    topics = list(QUESTION_BANK[class_selected][subject_selected].keys())
+    topics_selected = st.multiselect('Select Topics', topics)
+    generate = st.button('Generate Questions')
+    if generate and topics_selected:
+        questions = []
+        for topic in topics_selected:
+            available = QUESTION_BANK[class_selected][subject_selected][topic]
+            questions += random.sample(available, min(2, len(available)))
+        st.write("### Generated Questions")
+        for q in questions:
+            st.write("-", q)
+        if len(questions) > 0:
+            df = pd.DataFrame({'Questions': questions})
+            output = io.BytesIO()
+            with pd.ExcelWriter(output, engine='openpyxl') as writer:
+                df.to_excel(writer, index=False, sheet_name='Mock Questions')
+            st.download_button(label="Download Excel", data=output.getvalue(), file_name="ICSE_Mock_Questions.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    elif generate and not topics_selected:
+        st.warning("Please select at least one topic.")
+else:
+    st.info("Please wait for subject options to update.")
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    global generated_questions
-    classes = ['9', '10', '11', '12']
-    subjects = list(QUESTION_BANK['9'].keys())
-    topics = []
-    questions = []
-    selected_class = ''
-    selected_subject = ''
-    if request.method == 'POST':
-        selected_class = request.form['class']
-        selected_subject = request.form['subject']
-        topics = list(QUESTION_BANK[selected_class][selected_subject].keys())
-        if 'topics' in request.form:
-            selected_topics = request.form.getlist('topics')
-            questions = []
-            for topic in selected_topics:
-                questions += random.sample(QUESTION_BANK[selected_class][selected_subject][topic], 2)
-            generated_questions = questions
-    return render_template('index.html', 
-                           classes=classes, 
-                           subjects=subjects, 
-                           topics=topics, 
-                           questions=questions, 
-                           selected_class=selected_class, 
-                           selected_subject=selected_subject)
-
-@app.route('/download')
-def download():
-    global generated_questions
-    if not generated_questions:
-        return redirect(url_for('home'))
-    df = pd.DataFrame({'Questions': generated_questions})
-    output = io.BytesIO()
-    with pd.ExcelWriter(output, engine='openpyxl') as writer:
-        df.to_excel(writer, index=False, sheet_name='Mock Questions')
-    output.seek(0)
-    return send_file(output,
-                     download_name="ICSE_Mock_Questions.xlsx",
-                     as_attachment=True,
-                     mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-if __name__ == '__main__':
-    app.run()
+st.info("Expand the QUESTION_BANK dictionary for real use.")
